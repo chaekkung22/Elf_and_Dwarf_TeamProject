40f14a6e1fd3134ff87949735aa26bff4b44c27a
FlamingBun <wh180911@gmail.com>
2025-05-09 15:50:09 +0900

    feat: PauseUI 구현

--- Assets/00_Scripts/UI/PauseUI.cs
+++ Assets/00_Scripts/UI/PauseUI.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PauseUI : MonoBehaviour
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PauseUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button retryButton;
+    [SerializeField] private Button mainButton;
+    [SerializeField] private Button optionButton;
+
+    protected override void Initialize()
     {
-        
+        base.Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void SetUIActive(bool isActive)
     {
-        
+
+
+        this.gameObject.SetActive(isActive);
+    }
+
+    private void OnClickRetryButton()
+    {
+        // TODO: 스테이지씬 다시 호출
+        //SceneManager.LoadScene("StageScene")
+    }
+
+    private void OnClickMainButton()
+    {
+        // TODO: 메인씬 호출
+        // SceneManager.LoadScene("MainScene")
+    }
+
+    private void OnClickOptionButton()
+    {
+        // TODO: Option UI 만들면 호출
+        // UIManager.Instance.OpenUI(UIState.Option);
+    }
+
+    private void OnClickResumeButton()
+    {
+        // TODO: 게임 일시정지 해제
+        // StageManager.Instance.PauseGame();
     }
 }
