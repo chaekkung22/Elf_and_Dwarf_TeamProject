53ffc039f08ce9ba16209c560c9c145340f62e07
chae_kkung <ycy0109@naver.com>
2025-05-09 16:18:02 +0900

    feat : ExitGate 스크립트 수정

--- Assets/00_Scripts/Entity/Object/ExitGate.cs
+++ Assets/00_Scripts/Entity/Object/ExitGate.cs
@@ -6,9 +6,26 @@ public class ExitGate : MonoBehaviour, ICollisionStay
 {
 
     float spendTime = 1f;
-    
+    public bool isExitSucess = false;
+
+
     public void StayEvent(GameObject collider)
     {
         spendTime -= Time.deltaTime;
+
+        if (spendTime <= 0f)
+        {
+            spendTime = 0f;
+
+            if (IsCorrectType())
+                isExitSucess = true;  //UI에서 bool값을 받아 탈출시 나오는 화면 보여줘야할듯..?
+        }
     }
+
+    bool IsCorrectType()
+    {
+        //문의 타입과 플레이어의 타입이 맞는지 확인하는 과정 작성
+        return true;
+    }
+
 }
