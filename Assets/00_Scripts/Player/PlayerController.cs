79fd20e1be31524779af8147a8309a3583f26fe2
sindhon <tpgml2356@gmail.com>
2025-05-09 16:51:12 +0900

    feat: 플레이어 타입(불/물) 추가

--- Assets/00_Scripts/Player/PlayerController.cs
+++ Assets/00_Scripts/Player/PlayerController.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum PlayerType
+{
+    Fire,
+    Water
+}
+
 public class PlayerController : BaseController
 {
+    public PlayerType playerType;
+
     void OnMove(InputValue inputValue)
     {
         // 입력을 통한 플레이어의 이동 구현
@@ -14,6 +22,7 @@ public class PlayerController : BaseController
 
     void OnJump(InputValue inputValue)
     {
+        // 입력을 통한 플레이어의 점프 구현
         Jump();
     }
 }
