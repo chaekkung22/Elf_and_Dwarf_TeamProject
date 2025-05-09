376904612f394bc30e0e41bdb71bedd3e64bd142
chae_kkung <ycy0109@naver.com>
2025-05-09 17:46:24 +0900

    feat : JumpPad 방향에 맞춰서 충돌체가 받는 힘의 방향도 달라질 수 있도록 수정

--- Assets/00_Scripts/Entity/Object/JumpPad.cs
+++ Assets/00_Scripts/Entity/Object/JumpPad.cs
@@ -8,9 +8,14 @@ public class JumpPad : MonoBehaviour, ICollisionEnter
 
     public void EnterEvent(GameObject collider)
     {
+        //이 오브젝트 기준으로 위쪽. z가 회전이 되더라도 오브젝트의 기준으로 힘을 주기 위해 방향을 따옴
+        Vector2 direction = transform.up.normalized;
+        //Enter시에 한 번만 실행되기 때문에, 속도(velocity)에 적용해주기 위해 rigidbody를 들고 옴 
         Rigidbody rigidbody = collider.GetComponent<Rigidbody>();
 
         if (rigidbody != null)
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, force);  //x축으로 이동하던 속도는 유지, y에만 값을 넣어줌
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x + direction.x * force,
+                rigidbody.velocity.y + direction.y * force);  // 충돌한 오브젝트가 가지고 있던 기존 속도에 힘을 더해줌.
+                                                              // 만약 x값이 0(똑바로 위를 보는 상태)이면 x에는 힘이 더해지지 않음(기존 x축 속도 유지)
     }
 }
