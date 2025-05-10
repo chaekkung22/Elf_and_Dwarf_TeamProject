84012c850d96e9096d1599527fd9d1719dc8dbaa
kihyun <gosof@naver.com>
2025-05-10 23:35:56 +0900

    feat: 장판타입무시추가
    -장판타입무시 추가

--- Assets/00_Scripts/Entity/Object/ElementalObstacle.cs
+++ Assets/00_Scripts/Entity/Object/ElementalObstacle.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 //플레이어데스
 public class ElementalObstacle : MonoBehaviour, ICollisionEnter
 {
-    [SerializeField] private PlayerType oBSTACLETYPE;
+    [SerializeField] private PlayerType obstacleType;
+    [SerializeField] private bool ignoreElementalType = false;
 
     public void EnterEvent(GameObject collider)
     {
         PlayerController player;
         if (collider.TryGetComponent<PlayerController>(out player))
         {
-            if (player.PlayerType != oBSTACLETYPE)
+            if (!ignoreElementalType ||player.PlayerType != obstacleType)
             {
                 //플레이어데스
                 //player.OnDeath();
