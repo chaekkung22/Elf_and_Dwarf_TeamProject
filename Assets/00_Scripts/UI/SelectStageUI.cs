8a5c8bf28769a016908ca6d1a58c55b42ff0857c
hyeon11ok <hyeonsusin05@gmail.com>
2025-05-12 16:32:19 +0900

    feat : 스테이지 정보 출력 기능 구현
    - SelectStageUI에서 스테이지 정보를 받아와 UI에 출력해주는 기능 구현
    - 스테이지 정보 최초 생성 기능 구현 후 테스트 예정

--- Assets/00_Scripts/UI/SelectStageUI.cs
+++ Assets/00_Scripts/UI/SelectStageUI.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectStageUI : BaseUI
 {
+    [Header("Control UI")]
     [SerializeField] private Button selectStagePanel;
     [SerializeField] private Button stageSelectBtn;
+    [SerializeField] private Button nextBtn;
+    [SerializeField] private Button prevBtn;
+
+    [Header("Stage Info UI")]
+    [SerializeField] private GameObject[] starImgs;
+    [SerializeField] private TextMeshProUGUI bestTimeTxt;
+    [SerializeField] private TextMeshProUGUI gemCountTxt;
 
     protected override UIState UIState { get; } = UIState.SelectStage;
 
@@ -15,5 +24,25 @@ public class SelectStageUI : BaseUI
         base.Start();
         selectStagePanel.onClick.AddListener(UIManager.Instance.CloseUI);
         stageSelectBtn.onClick.AddListener(() => GameManager.Instance.ChangeScene("StageScene"));
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void ShowStageInfo(int stageId)
+    {
+        StageInfo info = DataManager.Instance.GetStageInfo();
+
+        // 별 최대 개수 3개
+        // 부족한 별 개수 만큼 오브젝트 비활성화
+        for(int i = 0; i < 3 - info.starCountList[stageId]; i++)
+        {
+            starImgs[i].SetActive(false);
+        }
+
+        bestTimeTxt.text = info.bestClearTimeList[stageId].ToString();
+        gemCountTxt.text = info.gemCountList[stageId].ToString();
     }
 }
