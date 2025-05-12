1c45c028ed2bb61d06561150e96290f427285ce0
chae_kkung <ycy0109@naver.com>
2025-05-12 09:04:46 +0900

    feat : 상점 스크립트 작성
    스크립트 작성중 ....

--- Assets/00_Scripts/UI/ShopUI.cs
+++ Assets/00_Scripts/UI/ShopUI.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ShopUI : MonoBehaviour
+public class ShopUI : BaseUI
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button prevButton;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button purchaseButton;
+    [SerializeField] private TextMeshProUGUI currentGoldText;
+    [SerializeField] private TextMeshProUGUI itemNameText;
+    [SerializeField] private TextMeshProUGUI itemPriceText;
+    //private ItemSO item;
+    private int currentPage = 1;
+    private int totalPage;
+
+
+    protected override UIState UIState { get; } = UIState.Shop;
+
+    protected override void Initialize()
     {
-        
+        base.Initialize();
+        //List<ItemSO> = DataManager.Instance.GetOwnedItems();
+        //ItemSO[] allItems = DataManager.Instance.GetAllItems();
+        //totalPage = allItems.Length / 3 + 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void SetUIActive(bool isActive)
     {
-        
+        base.SetUIActive(isActive);
+    }
+
+    void ShowCurrentPage()
+    {
+        //currentGoldText.text = DataManager.Instance.playerGold.ToString();
+        //itemNameText.text = item.itemID;
+        //itemPriceText.text = $"{item.Price.ToString()} 골드";
+    }
+
+    void PurchaseItem()
+    {
+        //if (DataManager.Instance.playerGold >= item.Price)
+        {
+        //DataManager.Instance.SpendGold(item.Price);
+        //DataManager.Instance.AddItem(itemID);
+        }
+    }
+
+
+    void PrevButton()
+    {
+
+    }
+
+    void NextButton()
+    {
+
     }
 }
