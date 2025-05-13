12e83687959360ede27da1ac98eb75519b0f077f
FlamingBun <wh180911@gmail.com>
2025-05-13 17:47:28 +0900

    fix : MainScene의 Button에 UI 연결 및 추가

--- Assets/00_Scripts/UI/QuestUI.cs
+++ Assets/00_Scripts/UI/QuestUI.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestUI : BaseUI
 {
+    [SerializeField] private Button exitButton;
     protected override UIState UIState { get; } = UIState.Quest;
 
     protected override void Initialize()
     {
         base.Initialize();
-
+        exitButton.onClick.RemoveAllListeners();
+        exitButton.onClick.AddListener(() => UIManager.Instance.CloseUI());
     }
 
     public override void SetUIActive(bool isActive)
