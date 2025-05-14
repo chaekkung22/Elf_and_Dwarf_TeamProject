419b0ee9505a4d2d98ec5f0d408ed6cd4bf14185
hyeon11ok <hyeonsusin05@gmail.com>
2025-05-15 03:08:26 +0900

    fix : 도전과제 UI 최신화 과정 오류 수정
    - onQuest 호출 지점 때문에 CheckInProgressQuestsCleared 메서드가 재귀함수처럼 동작하는 현상이 원인

--- Assets/00_Scripts/Quest/Quest.cs
+++ Assets/00_Scripts/Quest/Quest.cs
@@ -101,7 +101,6 @@ public class Quest : MonoBehaviour
             {
                 inProgressQuests.RemoveAt(i);
                 rewardAvailableQuests.Add(quest);
-                onQuest?.Invoke();
             }
         }
     }
