821764cb00c93b9716e923fea744a666f2e6483c
kihyun <gosof@naver.com>
2025-05-14 21:00:45 +0900

    fix: sound수정
    -sound수정

--- Assets/00_Scripts/Managers/SoundManager.cs
+++ Assets/00_Scripts/Managers/SoundManager.cs
@@ -1,50 +1,34 @@
-using System.Collections.Generic;
-using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
-using static SoundManager;
 
 public class SoundManager : Singleton<SoundManager>
 {
-    [SerializeField] AudioSource bgmAudioSource;
-    [SerializeField] AudioSource sfxAudioSource;
-    // 오디오 믹서
+    [SerializeField] private AudioSource bgmAudioSource;
+    [SerializeField] private AudioSource sfxAudioSource;
     public AudioMixer audioMixer;
-
-    // 슬라이더
-    [SerializeField] private Slider BgmSlider;
-    [SerializeField] private Slider SfxSlider;
-    [SerializeField] private Toggle BgmMute;    // Mute를 On / Off할 Toggle
-    [SerializeField] private Toggle SfxMute;	// Mute를 On / Off할 Toggle
-    private bool isChangingManually = false;
-
-    //GameManager
     [SerializeField] private SoundDataBaseManager soundDataBaseManager;
 
     protected override void Initialize()
     {
         soundDataBaseManager.Init();
-        // PlayerPrefs에 Volume 값이 저장되어 있을 경우,
-        LoadBgmVolume();
-        LoadSfxVolume();
-        // 슬라이더의 값이 변경될 때 AddListener를 통해 이벤트 구독
-        BgmSlider.onValueChanged.AddListener(SetBgmVolume);
-        SfxSlider.onValueChanged.AddListener(SetSfxVolume);
-        // 토클의 값이 변경될 때 AddListener를 통해 이벤트 구독
-        BgmMute.onValueChanged.AddListener(SetBgmMute);
-        SfxMute.onValueChanged.AddListener(SetSfxMute);
-
-        PlayBgm(BgmType.Main);
     }
     private void Start()
     {
-        // audioMixer.SetFloat("audioMixer에 설정해놓은 Parameter", float 값)
-        // audioMixer에 미리 설정해놓은 parameter 값을 변경하는 코드.
-        // Mathf.Log10(BGMSlider.value) * 20 : 데시벨이 비선형적이기 때문에 해당 방식으로 값을 계산.
-        audioMixer.SetFloat("BGM", Mathf.Log10(BgmSlider.value) * 20);
-        audioMixer.SetFloat("SFX", Mathf.Log10(SfxSlider.value) * 20);
+        LoadVolumes();
+        PlayBgm(BgmType.Main);
     }
+    private void LoadVolumes()
+    {
+        float bgmVolume = PlayerPrefs.GetFloat("BgmVolume", 0.5f);
+        float sfxVolume = PlayerPrefs.GetFloat("SfxVolume", 0.5f);
+
+        bgmVolume = Mathf.Max(bgmVolume, 0.0001f);
+        sfxVolume = Mathf.Max(sfxVolume, 0.0001f);
+
+        audioMixer.SetFloat("BGM", Mathf.Log10(bgmVolume) * 20);
+        audioMixer.SetFloat("SFX", Mathf.Log10(sfxVolume) * 20);
+    }
+
     public void PlayBgm(BgmType bgmType)
     {
         bgmAudioSource.clip = soundDataBaseManager.GetBgmData(bgmType).clip;
@@ -58,127 +42,54 @@ public class SoundManager : Singleton<SoundManager>
 
     public void SetBgmVolume(float value)
     {
-
-        if (Mathf.Approximately(BgmSlider.value, 0f))
-        {
-            isChangingManually = true;
-            BgmMute.isOn = true;
-            isChangingManually = false;
-        }
-        else if (!isChangingManually)
-        {
-            isChangingManually = true;
-            BgmMute.isOn = false;
-            isChangingManually = false;
-        }
-        // 로그 연산 값 전달
-        audioMixer.SetFloat("BGM", Mathf.Log10(BgmSlider.value) * 20);
-        PlayerPrefs.SetFloat("BgmVolume", BgmSlider.value);
-    }
-    public void SetBgmMute(bool mute)
-    {
-        if (isChangingManually) return;
-
-        isChangingManually = true;
-        if (mute)
-        {
-            PlayerPrefs.SetFloat("BgmVolumeBeforeMute", BgmSlider.value);
-            BgmSlider.value = 0;
-        }
-        else
-        {
-            float saved = PlayerPrefs.GetFloat("BgmVolumeBeforeMute", 0.5f);
-            BgmSlider.value = saved == 0 ? 0.5f : saved;
-        }
-        PlayerPrefs.SetString("BgmMute", mute.ToString());
-        isChangingManually = false;
+        value = Mathf.Max(value, 0.0001f);
+        audioMixer.SetFloat("BGM", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("BgmVolume", value);
     }
 
-    public void SetSfxMute(bool mute)
-    {
-        if (isChangingManually) return;
-
-        isChangingManually = true;
-        if (mute)
-        {
-            PlayerPrefs.SetFloat("SfxVolumeBeforeMute", SfxSlider.value);
-            SfxSlider.value = 0;
-        }
-        else
-        {
-            float saved = PlayerPrefs.GetFloat("SfxVolumeBeforeMute", 0.5f);
-            SfxSlider.value = saved == 0 ? 0.5f : saved;
-        }
-        PlayerPrefs.SetString("SfxMute", mute.ToString());
-        isChangingManually = false;
-    }
     public void SetSfxVolume(float value)
     {
-
-        if (Mathf.Approximately(SfxSlider.value, 0f))
-        {
-            isChangingManually = true;
-            SfxMute.isOn = true;
-            isChangingManually = false;
-        }
-        else if (!isChangingManually)
-        {
-            isChangingManually = true;
-            SfxMute.isOn = false;
-            isChangingManually = false;
-        }
-        // 로그 연산 값 전달
-        audioMixer.SetFloat("SFX", Mathf.Log10(SfxSlider.value) * 20);
-        PlayerPrefs.SetFloat("SfxVolume", SfxSlider.value);
+        value = Mathf.Max(value, 0.0001f);
+        audioMixer.SetFloat("SFX", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SfxVolume", value);
     }
 
-    private void LoadBgmVolume()
+    public void MuteBgm(bool mute)
     {
-        if (PlayerPrefs.HasKey("BgmVolume"))
+        if (mute)
         {
-            // Slider의 값을 저장해 놓은 값으로 변경.
-            BgmSlider.value = PlayerPrefs.GetFloat("BgmVolume");
-
-            audioMixer.SetFloat("BGM", Mathf.Log10(BgmSlider.value) * 20);
+            float saved = PlayerPrefs.GetFloat("BgmVolume", 0.5f);
+            PlayerPrefs.SetFloat("BgmVolumeBeforeMute", saved);
+            SetBgmVolume(0.0001f);
         }
         else
-            BgmSlider.value = 0.5f;     // PlayerPrefs에 Volume이 없을 경우
-
-        if (PlayerPrefs.HasKey("BgmMute"))
         {
-            if (PlayerPrefs.GetString("BgmMute") == "true")
-            {
-                BgmMute.isOn = true;
-            }
-            else
-                BgmMute.isOn = false;
+            float restored = PlayerPrefs.GetFloat("BgmVolumeBeforeMute", 0.5f);
+            SetBgmVolume(restored);
         }
-        else
-            BgmMute.isOn = false;
+
+        PlayerPrefs.SetInt("BgmMuted", mute ? 1 : 0);
     }
-    private void LoadSfxVolume()
+
+    public void MuteSfx(bool mute)
     {
-        if (PlayerPrefs.HasKey("SfxVolume"))
+        if (mute)
         {
-            // Slider의 값을 저장해 놓은 값으로 변경.
-            SfxSlider.value = PlayerPrefs.GetFloat("SfxVolume");
-            audioMixer.SetFloat("SFX", Mathf.Log10(SfxSlider.value) * 20);
+            float saved = PlayerPrefs.GetFloat("SfxVolume", 0.5f);
+            PlayerPrefs.SetFloat("SfxVolumeBeforeMute", saved);
+            SetSfxVolume(0.0001f);
         }
         else
-            SfxSlider.value = 0.5f;     // PlayerPrefs에 Volume이 없을 경우
+        {
+            float restored = PlayerPrefs.GetFloat("SfxVolumeBeforeMute", 0.5f);
+            SetSfxVolume(restored);
+        }
 
-        if (PlayerPrefs.HasKey("SfxMute"))
-        {
-            if (PlayerPrefs.GetString("SfxMute") == "true")
-            {
-                SfxMute.isOn = true;
-            }
-            else
-                SfxMute.isOn = false;
-        }
-        else
-            SfxMute.isOn = false;
+        PlayerPrefs.SetInt("SfxMuted", mute ? 1 : 0);
     }
 
-
+    public bool IsBgmMuted() => PlayerPrefs.GetInt("BgmMuted", 0) == 1;
+    public bool IsSfxMuted() => PlayerPrefs.GetInt("SfxMuted", 0) == 1;
+    public float GetBgmVolume() => PlayerPrefs.GetFloat("BgmVolume", 0.5f);
+    public float GetSfxVolume() => PlayerPrefs.GetFloat("SfxVolume", 0.5f);
 }
--- Assets/00_Scripts/UI/OptionUI.cs
+++ Assets/00_Scripts/UI/OptionUI.cs
@@ -1,18 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OptionUI : BaseUI
 {
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private Toggle bgmMuteToggle;
+    [SerializeField] private Toggle sfxMuteToggle;
     [SerializeField] private Button okButton;
 
-    protected override UIState UIState { get; } = UIState.Option;
+    protected override UIState UIState => UIState.Option;
+    private bool isChangingManually = false;
 
     protected override void Initialize()
     {
         base.Initialize();
-        okButton.onClick.RemoveAllListeners();
+
+        bgmSlider.onValueChanged.AddListener(OnBgmSliderChanged);
+        sfxSlider.onValueChanged.AddListener(OnSfxSliderChanged);
+        bgmMuteToggle.onValueChanged.AddListener(OnBgmMuteChanged);
+        sfxMuteToggle.onValueChanged.AddListener(OnSfxMuteChanged);
         okButton.onClick.AddListener(() => UIManager.Instance.CloseUI());
+
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        isChangingManually = true;
+
+        bgmSlider.value = SoundManager.Instance.GetBgmVolume();
+        sfxSlider.value = SoundManager.Instance.GetSfxVolume();
+        bgmMuteToggle.isOn = SoundManager.Instance.IsBgmMuted();
+        sfxMuteToggle.isOn = SoundManager.Instance.IsSfxMuted();
+
+        isChangingManually = false;
+    }
+
+    private void OnBgmSliderChanged(float value)
+    {
+        if (isChangingManually) return;
+
+        SoundManager.Instance.SetBgmVolume(value);
+        isChangingManually = true;
+        bgmMuteToggle.isOn = Mathf.Approximately(value, 0f);
+        isChangingManually = false;
+    }
+
+    private void OnSfxSliderChanged(float value)
+    {
+        if (isChangingManually) return;
+
+        SoundManager.Instance.SetSfxVolume(value);
+        isChangingManually = true;
+        sfxMuteToggle.isOn = Mathf.Approximately(value, 0f);
+        isChangingManually = false;
+    }
+
+    private void OnBgmMuteChanged(bool isMuted)
+    {
+        if (isChangingManually) return;
+
+        SoundManager.Instance.MuteBgm(isMuted);
+        isChangingManually = true;
+        bgmSlider.value = isMuted ? 0f : SoundManager.Instance.GetBgmVolume();
+        isChangingManually = false;
+    }
+
+    private void OnSfxMuteChanged(bool isMuted)
+    {
+        if (isChangingManually) return;
+
+        SoundManager.Instance.MuteSfx(isMuted);
+        isChangingManually = true;
+        sfxSlider.value = isMuted ? 0f : SoundManager.Instance.GetSfxVolume();
+        isChangingManually = false;
     }
 }
