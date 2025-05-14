1db17389a1181ed980913b0a2941a993e521cc8f
hyeon11ok <hyeonsusin05@gmail.com>
2025-05-14 14:43:03 +0900

    refact : ActiveRule 수정
    - class에서 struct로 변경

--- Assets/00_Scripts/Entity/Object/WindObstacle.cs
+++ Assets/00_Scripts/Entity/Object/WindObstacle.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class ActiveRule
+public struct ActiveRule
 {
     public InteractObj interactObj;
     [SerializeField] private bool rule;
