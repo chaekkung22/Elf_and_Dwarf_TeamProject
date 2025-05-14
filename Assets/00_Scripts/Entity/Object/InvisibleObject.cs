f3fd81566636879716a05822f00193e43eba2905
hyeon11ok <hyeonsusin05@gmail.com>
2025-05-14 15:54:15 +0900

    feat : 활성화 이벤트 오브젝트 기능 구현
    - 활성화 이벤트 오브젝트 기능 구현

---
+++ Assets/00_Scripts/Entity/Object/InvisibleObject.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+using UnityEngine.UIElements;
+
+public class InvisibleObject : MonoBehaviour
+{
+    [SerializeField] private List<ActiveRule> rules = new List<ActiveRule>();
+
+    private Renderer _renderer;
+    private Collider2D _collider;
+
+    [SerializeField] private bool invisibleOnAwake;
+
+    private void Start()
+    {
+        _renderer = GetComponentInChildren<Renderer>();
+        _collider = GetComponentInChildren<Collider2D>();
+
+        for(int i = 0; i < rules.Count; i++)
+        {
+            rules[i].interactObj.OnEventOn += CheckEvent;
+            rules[i].interactObj.OnEventOff += CheckEvent;
+        }
+    }
+
+    private void ChangeInvisibleMode(bool enabled)
+    {
+        if(_renderer != null)
+            _renderer.enabled = enabled;
+        if(_collider != null)
+            _collider.enabled = enabled;
+    }
+
+    public void CheckEvent()
+    {
+        bool isSatisfied = true;
+
+        for(int i = 0; i < rules.Count; i++)
+        {
+            if(!rules[i].CheckingRule())
+                isSatisfied = false;
+        }
+
+        if(isSatisfied)
+        {
+            EventOn();
+        }
+        else
+        {
+            EventOff();
+        }
+    }
+
+    private void EventOn()
+    {
+        if(invisibleOnAwake)
+            ChangeInvisibleMode(false);
+        else
+            ChangeInvisibleMode(true);
+    }
+
+    private void EventOff()
+    {
+        if(invisibleOnAwake)
+            ChangeInvisibleMode(true);
+        else
+            ChangeInvisibleMode(false);
+    }
+}
